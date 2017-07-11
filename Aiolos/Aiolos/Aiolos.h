30266fad104262fb0bee356853f843db8a8a09e0
Matthias <matthias@ideasoncanvas.com>
2017-07-11 15:25:52 +0200

    Add empty project

---
+++ Aiolos/Aiolos/Aiolos.h
@@ -0,0 +1,19 @@
+//
+//  Aiolos.h
+//  Aiolos
+//
+//  Created by Matthias Tretter on 11/07/2017.
+//  Copyright © 2017 Matthias Tretter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aiolos.
+FOUNDATION_EXPORT double AiolosVersionNumber;
+
+//! Project version string for Aiolos.
+FOUNDATION_EXPORT const unsigned char AiolosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aiolos/PublicHeader.h>
+
+
---
+++ Aiolos/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aiolos
+//
+//  Created by Matthias Tretter on 11/07/2017.
+//  Copyright © 2017 Matthias Tretter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
