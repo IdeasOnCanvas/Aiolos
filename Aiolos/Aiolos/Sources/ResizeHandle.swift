4efeb3553cc1c479ab9cc83d3deb1c1aa7e9fa64
Matthias <matthias@ideasoncanvas.com>
2020-03-31 22:23:31 +0200

    Replace manually drawn resize handle with UIButton, to simplify pointer interaction

--- Aiolos/Aiolos/Sources/ResizeHandle.swift
+++ Aiolos/Aiolos/Sources/ResizeHandle.swift
@@ -15,13 +15,12 @@ public final class ResizeHandle: UIView {
     public struct Constants {
         public static let height: CGFloat = 20.0
 
-        fileprivate static let defaultWidth: CGFloat = 32.0
-        fileprivate static let resizeWidth: CGFloat = 38.0
-        fileprivate static let pointerWidth: CGFloat = 62.0
         fileprivate static let handleHeight: CGFloat = 5.0
+        fileprivate static let inactiveHandleWidth: CGFloat = 38.0
+        fileprivate static let activeHandleWidth: CGFloat = 44.0
     }
 
-    private lazy var resizeHandle: CAShapeLayer = self.makeResizeHandle()
+    private lazy var resizeHandle: UIView = self.makeResizeHandle()
 
     // MARK: - Properties
 
@@ -35,8 +34,9 @@ public final class ResizeHandle: UIView {
         didSet {
             guard oldValue != self.isResizing else { return }
 
-            self.updateResizeHandlePath(animated: true)
+            self.updateResizeHandleFrame(animated: true)
             self.updateResizeHandleColor()
+            self.setNeedsLayout()
         }
     }
 
@@ -54,17 +54,19 @@ public final class ResizeHandle: UIView {
         self.clipsToBounds = false
         self.isOpaque = false
         self.backgroundColor = .clear
-        self.layer.addSublayer(self.resizeHandle)
+        self.addSubview(self.resizeHandle)
         self.configure(with: configuration)
-
-        if #available(iOS 13.4, *) {
-            let pointerInteraction = UIPointerInteraction(delegate: self)
-            self.addInteraction(pointerInteraction)
-        }
     }
 
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UIView
+
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        self.updateResizeHandleFrame(animated: false)
     }
 
     // MARK: - ResizeHandle
@@ -74,7 +76,7 @@ public final class ResizeHandle: UIView {
 
         self.handleColor = foregroundColor
         self.backgroundColor = backgroundColor
-        self.resizeHandle.opacity = configuration.gestureResizingMode != .disabled && configuration.supportedModes.count > 1 ? 1.0 : 0.2
+        self.resizeHandle.alpha = configuration.gestureResizingMode != .disabled && configuration.supportedModes.count > 1 ? 1.0 : 0.2
     }
 }
 
@@ -87,82 +89,44 @@ extension ResizeHandle {
     }
 }
 
-// MARK: - UIView
-
-extension ResizeHandle {
-
-    override public func layoutSubviews() {
-        super.layoutSubviews()
-
-        self.resizeHandle.frame = self.bounds
-        self.updateResizeHandlePath()
-    }
-}
-
-// MARK: - UIPointerInteractionDelegate
-
-@available(iOS 13.4, *)
-extension ResizeHandle: UIPointerInteractionDelegate {
-
-    public func pointerInteraction(_ interaction: UIPointerInteraction, regionFor request: UIPointerRegionRequest, defaultRegion: UIPointerRegion) -> UIPointerRegion? {
-        let pointerRect = CGRect(center: CGPoint(x: self.bounds.width / 2.0, y: self.bounds.height / 2.0),
-                                 size: CGSize(width: Constants.pointerWidth, height: Constants.handleHeight + 12.0))
-
-        return UIPointerRegion(rect: pointerRect)
-    }
-
-    public func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
-        return UIPointerStyle(effect: .automatic(.init(view: self)))
-    }
-}
-
 // MARK: - Private
 
 private extension ResizeHandle {
 
-    func makeResizeHandle() -> CAShapeLayer {
-        let layer = CAShapeLayer()
-        layer.contentsScale = UIScreen.main.scale
-        layer.backgroundColor = UIColor.clear.cgColor
-        layer.fillColor = UIColor.clear.cgColor
-        layer.lineWidth = Constants.handleHeight
-        layer.lineCap = .round
-        return layer
+    func makeResizeHandle() -> UIView {
+        let handle = UIButton(type: .custom)
+        handle.frame.size = CGSize(width: Constants.inactiveHandleWidth, height: Constants.handleHeight)
+        handle.backgroundColor = self.handleColor
+        if #available(iOS 13.4, *) {
+            handle.isPointerInteractionEnabled = true
+        }
+        return handle
     }
 
     func updateResizeHandleColor() {
         let baseColor = self.handleColor
-        self.resizeHandle.strokeColor = self.isResizing ? baseColor.darkened().cgColor : baseColor.cgColor
+        self.resizeHandle.backgroundColor = self.isResizing ? baseColor.darkened() : baseColor
     }
 
-    func updateResizeHandlePath(animated: Bool = false) {
-        func makeHandlePath(width: CGFloat) -> UIBezierPath {
-            let r = self.bounds.divided(atDistance: 13.0, from: .maxYEdge).slice
-            let y = r.minY + Constants.handleHeight / 2.0
+    func updateResizeHandleFrame(animated: Bool) {
+        let width = self.isResizing ? Constants.activeHandleWidth : Constants.inactiveHandleWidth
 
-            let boundingRect = CGRect(center: .init(x: r.width / 2.0, y: y), size: .init(width: width, height: 0.0))
-            let path = UIBezierPath()
-            path.move(to: boundingRect.minXmidY)
-            path.addLine(to: boundingRect.maxXmidY)
-            return path
+        func updateFrame() {
+            let center = CGPoint(x: self.bounds.width / 2.0, y: self.bounds.height / 2.0 - 0.5)
+            self.resizeHandle.frame = CGRect(center: center, size: .init(width: width, height: Constants.handleHeight))
+            self.resizeHandle.layer.cornerRadius = self.resizeHandle.bounds.height / 2.0
         }
-
-        let path = makeHandlePath(width: self.isResizing ? Constants.resizeWidth : Constants.defaultWidth)
 
         if animated {
-            self.addAnimation(to: self.resizeHandle)
+            UIView.animate(withDuration: 0.3,
+                           delay: 0.0,
+                           usingSpringWithDamping: 1.0,
+                           initialSpringVelocity: 0.0,
+                           options: .curveLinear,
+                           animations: updateFrame)
+        } else {
+            updateFrame()
         }
-        self.resizeHandle.path = path.cgPath
-    }
-
-    func addAnimation(to layer: CAShapeLayer) {
-        let animationKey = "pathAnimation"
-        let animation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.path))
-        animation.duration = 0.3
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-
-        layer.removeAnimation(forKey: animationKey)
-        layer.add(animation, forKey: animationKey)
     }
 }
 
@@ -183,9 +147,6 @@ private extension UIColor {
 
 
 private extension CGRect {
-
-    var minXmidY: CGPoint { .init(x: self.minX, y: self.midY) }
-    var maxXmidY: CGPoint { .init(x: self.maxX, y: self.midY) }
 
     init(center: CGPoint, size: CGSize) {
         self.init(x: center.x - size.width / 2.0, y: center.y - size.height / 2.0, width: size.width, height: size.height)
